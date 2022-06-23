16669f28e450cae372ffddd61596649b503a98fe
Joshua Cheng <joshua-admin@Poco-Coco.attlocal.net>
2022-06-22 19:46:54 -0700

    pretty much finished all the styling, had to play around a lot with flexbox to get everything where it needed to be, also added the fonts and colors for most stuff in the entry component. Next need to finish off any font colors and then edit the data to add some real entriesgit add .

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,18 +1,44 @@
 import React from "react";
+import {MapPin} from "phosphor-react"
+
+
 export default function Entry(props)
 {
     function viewOnMaps(){
         window.open(props.item.googleMapsUrl);
     }
+    function three()
+    {
+        return(
+            <div className="location-line">
+
+            <MapPin size={15} color="#F55A5A" fill="#F55A5A" className="pin" />
+
+            <p className="location">{props.item.location}</p>
+
+            <p className="map-link"  onClick={viewOnMaps}>View on Google Maps</p>
+            </div>
+        )
+    }
     return(
         <div>
-            <p>{props.item.location}</p>
-            <p className="map-link"  onClick={viewOnMaps}>View on Google Maps</p>
-            <h3>{props.item.title}</h3>
+        <div className="entry">
+            <img className ="entry-image" src={props.item.imageUrl}/>
+         <div className="info">
+            {three()}
+            <h1 className="title">{props.item.title}</h1>
+            
+            <h4>{props.item.startDate}-{props.item.endDate}</h4>
+            <h3 className="desc">{props.item.description}</h3>
+            
 
+           
+            </div>
+           
 
         </div>
+         <hr/>
+         </div>
 
     )
-
 }
